e96ce4e2a1c67894081151bd57c7b406f5963e1a
Dhruv-487 <ranadhruv487@gmail.com>
2025-06-12 15:55:42 +0000

    datatype

---
+++ datatypes-summary.js
@@ -0,0 +1,48 @@
+// the diff is on the basis of how these data are stored in memory and how can we access it.
+
+//  #Primitive-these are call by value , copy krke dia jata hai
+
+
+// 7 types/categories:String , Number , Boolean , null , undefined , Symbol , BigInt.
+
+
+// JavaScript is dynamically typed, not statically typed.
+// Dynamically typed means you don't need to declare variable types. The type is determined at runtime.
+const outsideTemp=null
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+// console.log(id===anotherId);//false
+// jabki values hmne same di hai but return value is not same as names are diff this is what symbol do and that's how it is used for uniqueness
+
+
+const bigNumber=141561460158916580615n
+// last me n lgane se Big Int ban gaya hai
+
+
+// **********************************************************//
+
+
+//Reference(Non Primitive)
+// Array,Objects,Functions
+
+const heroes=["Shaktiman","spiderman","batman"];
+
+let myObj={
+    name:"dhruv",
+    age:19,
+}
+// {} ke andr ka values is object
+
+const myFunction=function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);//"object" return krega  null ka type
+
+console.log(typeof myFunction);
+
+// jitte bhi non primitive type hai inka return type object is aata hai,
+// idhr function ka function hi ara hai but in actual it is function-object
