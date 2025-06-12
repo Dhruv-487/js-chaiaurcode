cce36b23d24fba8b28a015183377d799c00d7140
Dhruv-487 <ranadhruv487@gmail.com>
2025-06-12 09:33:37 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") //we are using node js,not browser thats why not running here
+
+
+// code readability should be high
+console.log(3+3)
+
+console.log("Dhruv")
+
+let name="dhruv"
+let age=18
+let isLoggedIn=false
+
+
+// Primitive DataTypes
+//number => 2^53
+//bigint - when big number
+//string => "" prefer double quotes
+//boolean=>true/false
+//null=>standalone value,this is declared but empty 
+        // we can assign some  var to null 
+//undefined=>var is declared but value is not defined/assigned
+//symbol=>unique
+
+
+
+// object
+console.log(typeof "dhruv")
+console.log(typeof age);
+console.log(typeof null) //this gives output as object
+console.log(typeof undefined); //undefined
+
