7369cc7c6af64933d74a2a6a44b1041cebecb659
Dhruv-487 <ranadhruv487@gmail.com>
2025-07-28 15:09:23 +0000

    static prop

--- 10_classes_and_oop/staticProp.js
+++ 10_classes_and_oop/staticProp.js
@@ -6,19 +6,23 @@ class User{
         console.log(`Username: ${this.username}`);
     }
 
+    //static krdia to koi access ni kr payega directly
     static createID(){
         return `123`
     }
 }
+
 const hitesh=new User("hitesh")
-// console.log(hitesh.createID()) 
+// console.log(hitesh.createID()) //no o/p
 
 class Teacher extends User{
     constructor(username,email){
-        super(username)
+        super(username)//this will do everything on its own
         this.email=email
     }
 }
+
 const iphone=new Teacher("iphone","i@phone.com")
 iphone.logMe()
-console.log(iphone.createID())//static vala access ni kr skte hm
+// console.log(iphone.createID())  //static vala access ni kr skte hm
+console.log(iphone)
