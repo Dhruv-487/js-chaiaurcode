e6b5e46a4135bd0b1e8fcfe6144438cbb8590f05
Dhruv-487 <ranadhruv487@gmail.com>
2025-08-12 08:15:25 +0000

    err

---
+++ Course/Lec89/server.js
@@ -0,0 +1,46 @@
+const http=require('http')
+const fs=require("fs");
+const path=require('path')
+
+const port=3000
+
+const server=http.createServer((req,res)=>{
+    if (req.url === '/about') {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("404: File Not Found Bro");
+        return;
+    }
+    const filePath=path.join(__dirname,req.url==='/' ? "index.html" : req.url)
+    console.log(filePath);
+
+    const extName=String(path.extname(filePath)).toLowerCase()
+
+    const mimeTypes={
+        '.html':'text/html',
+        '.css': 'text/css',
+        '.js':  'text/javascript',
+        '.png': 'text/png',
+    }
+    const contentType=mimeTypes[extName] || 'application/octet-stream';
+
+    fs.readFile(filePath,(err,content)=>{
+        if(err){
+            if(err.code==="ENOENT"){
+                res.writeHead(404,{"Content-Type":"text/html"});
+                res.end("404:File Not Found Bro");
+            }
+        }
+        else{
+            res.writeHead(200,{'Content-Type':contentType});
+            res.end(content,"utf-8");
+        }
+    });
+});
+
+
+
+server.listen(port,(req,res)=>{
+    console.log(`Server  is listening on port ${port}`)
+});
+
+//here we have created a server and its just listening
